f3bb2f2a61a2c9ef0f13f6504951ba479e8b6034
Shree <ubuntu-clone@example.com>
2022-09-27 18:18:47 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express()
+
+app.listen(3000, () => {
+    console.log('Listening on Port 3000');
+
+})
+
+
+app.get('/',(req,res) => {
+    res.json({message: "API is Working "})
+})
+
+app.get('/api/students', (req,res) => {
+    res.json(students)
+})
+
+app.post('/api/students',(req,res) => {
+
+
+    if(!req.body.email){
+        res.status(400)
+        return res.json({error : "email is required..."})
+    }
+    const user = {
+        id : students.length + 1,
+        first_name : req.body.first_name,
+        last_name : req.body.last_name,
+        email : req.body.email
+        
+    }
+
+    students.push(user)
+    res.send("user")
+})
+
+app.put('/api/students',(req,res) => {
+    const id : students.length + 1,
+    const first_name : req.body.first_name,
+    const last_name : req.body.last_name,
+    const email : req.body.email
